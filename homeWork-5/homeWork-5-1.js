c6094e53c9e750b9c60fbfce8f376890df3fce56
AlexeyKobevko <folco_2000@mail.ru>
2018-12-06 12:02:46 +0300

    Сделал 1-е задание 5-го урока

---
+++ homeWork-5/homeWork-5-1.js
@@ -0,0 +1,52 @@
+'use strict';
+
+/**
+ * @property {HTMLElement} gameContainerEl Контейнер игры (DOM элемент).
+ */
+const chess = {
+  gameContainerEl: document.getElementById('game'),
+  /**
+   * Метод отображения карты (игрового поля).
+   */
+  renderMap() {
+    const cols = [0, 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 0];
+    const rows = [0, '8', '7', '6', '5', '4', '3', '2', '1', 0];
+
+    for (let row = 0; row < 10; row++) {
+      const tr = document.createElement('tr');
+      this.gameContainerEl.appendChild(tr);
+
+      for (let col = 0; col < 10; col++) {
+          const td = document.createElement('td');
+          tr.appendChild(td);
+
+          if ((row === 0 || row === rows.length - 1) && cols[col] !== 0) {
+              td.innerText = cols[col];
+          }
+
+          if ((col === 0 || col === cols.length - 1) && rows[row] !== 0) {
+              td.innerText = rows[row];
+          }
+
+          if (this.isCellIsBlack(row, col) && rows[row] !== 0 && cols[col] !== 0) {
+              td.style.backgroundColor = 'grey';
+          }
+      }
+    }
+  },
+  /**
+   * Определяет является ли ячейка черной.
+   * @param {int} rowNum Номер в строке.
+   * @param {int} colNum Номер в колонке.
+   * @returns {boolean} true, если ячейка должна быть черной, иначе false.
+   */
+  isCellIsBlack(rowNum, colNum) {
+      if ((rowNum % 2 === 0 && colNum % 2 === 0) || (rowNum % 2 !== 0 && colNum % 2 !== 0)) {
+          return false;
+      } else return true;
+
+  },
+};
+
+// Запускаем метод отображения карты.
+chess.renderMap();
